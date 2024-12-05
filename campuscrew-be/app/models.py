594cdb86b7cecf93e1284ae4250ad88ca3cfdab7
pfw_Jayasri_Suresh_Vani <surej01@pfw.edu>
2024-12-04 19:48:18 -0500

    Create User Operation

---
+++ campuscrew-be/app/models.py
@@ -0,0 +1,35 @@
+from werkzeug.security import generate_password_hash, check_password_hash
+import os
+from supabase import create_client
+import uuid
+from datetime import datetime
+
+supabase = create_client(os.environ.get('SUPABASE_URL'), os.environ.get('SUPABASE_KEY'))
+
+class Users:
+    @staticmethod
+    def create_user(email, password, first_name, last_name, user_id, is_pfw):
+        hashed_password = generate_password_hash(password)
+        response = supabase.table("users").insert({
+            "email": email,
+            "password": hashed_password,
+            "first_name": first_name,
+            "last_name": last_name,
+            "user_id": user_id,
+            "is_pfw": is_pfw
+        }).execute()
+        print(response)
+        if not response.data:
+            return None, f"Failed to create user: {response.data}"
+        return response.data, None
+
+    @staticmethod
+    def find_by_email(email):
+        response = supabase.table("users").select("*").eq("email", email).execute()
+        if not response or len(response.data) == 0:
+            return None
+        return response.data[0]
+
+    @staticmethod
+    def check_password(user, password):
+        return check_password_hash(user["password"], password)
